278b3a7e905f1ce3b88e25c5aacce684ecd4d6d9
Mihail Uymaz <michael.m.uymaz@gmail.com>
2023-11-22 00:23:14 +0200

    style: NoPageFound

--- messenger/src/views/NoPageFound/NoPageFound.tsx
+++ messenger/src/views/NoPageFound/NoPageFound.tsx
@@ -9,23 +9,23 @@ const NoPageFound = (): JSX.Element => {
       m={'auto'}
       w={{ base: '80%', md: '80%', lg: '50%' }}
       borderRadius={'10px'}
-      status='error'
-      variant='subtle'
-      flexDirection='column'
-      alignItems='center'
-      justifyContent='center'
-      textAlign='center'
-      height='200px'
+      status={'error'}
+      variant={'subtle'}
+      flexDirection={'column'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      textAlign={'center'}
+      height={'200px'}
     >
-      <AlertIcon boxSize='40px' mr={0} />
-      <AlertTitle mt={4} mb={1} fontSize='lg'>
+      <AlertIcon boxSize={'40px'} mr={0} />
+      <AlertTitle mt={4} mb={1} fontSize={'lg'}>
         404 page not found
       </AlertTitle>
-      <AlertDescription maxWidth='sm'>
+      <AlertDescription maxWidth={'sm'}>
         Uh-oh! We&apos;re not sure what you are looking for...
       </AlertDescription>
-      <AlertDescription maxWidth='sm'>
-        `Return to <Link fontWeight={'bold'} color={'blue'} onClick={() => navigate('/')}>Home Page</Link>?
+      <AlertDescription maxWidth={'sm'}>
+        Return to <Link fontWeight={'bold'} color={'blue'} onClick={() => navigate('/')}>Home Page</Link>?
       </AlertDescription>
     </Alert>
   )
