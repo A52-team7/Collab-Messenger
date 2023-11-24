e6f727fee439506d51111f98871f502975f0eb12
Mihail Uymaz <michael.m.uymaz@gmail.com>
2023-11-24 04:03:09 +0200

    css: Body, SidebarContent, NavItem, main.tsx
    Test style for more userfriendly navigation on mobile devices. Structural changes added as well.

--- messenger/src/components/SidebarContent/SidebarContent.tsx
+++ messenger/src/components/SidebarContent/SidebarContent.tsx
@@ -4,20 +4,24 @@ import NavItem from '../NavItem/NavItem';
 import {
   Box,
   Flex,
-  Text,
+  Button,
   CloseButton,
   useColorModeValue,
-  Image,
+  Heading
 } from '@chakra-ui/react';
 import {
   FiHome,
 } from 'react-icons/fi';
+import { BsChatTextFill } from "react-icons/bs";
+import { RiTeamFill } from "react-icons/ri";
+
+
 import UserTeams from '../UserTeams/UserTeams'
 
 const LinkItems = [
   { name: 'Home', icon: FiHome, path: '/' },
   { name: 'Team', icon: FiHome, path: '/new-team' },
-  {name:'Create new chat', icon: FiHome, path: '/create-new-chat'},
+  { name: 'Create new chat', icon: FiHome, path: '/create-new-chat' },
 ];
 
 interface SidebarContentProps {
@@ -29,26 +33,29 @@ const SidebarContent = ({ onClose, ...rest }: SidebarContentProps) => {
   const { userData } = useContext(AppContext);
   return (
     <Box
-      transition='3s ease'
+      transition={'3s ease'}
       bg={'blue'}
-      borderRight='1px'
+      borderRight={'1px'}
       borderRightColor={useColorModeValue('gray.200', 'gray.700')}
-      w={{ base: 'full', md: 60 }}
-      pos='fixed'
-      h='full'
+      w={{ base: 'fit-content', md: 60 }}
+      pos={'fixed'}
+      h={'full'}
       {...rest}>
-      <Flex alignItems='center' justify="center">
-        <Image alt='logo' w={"150px"} alignItems='center' />
-      </Flex>
-      <Flex h='20' alignItems='center' mx='4' justifyContent='space-between'>
-        <Text fontSize='23' fontFamily='monospace' fontWeight='bold' color={'grey'}>
-          COLLAB-MESSANGER
-        </Text>
+      <Flex alignItems={'flex-end'} mx='4' justifyContent={'flex-end'}>
         <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
       </Flex>
-      <NavItem key={LinkItems[0].name} icon={LinkItems[0].icon} name={LinkItems[0].name} path={LinkItems[0].path} />
-      <UserTeams/>
-      <NavItem key={LinkItems[2].name} icon={LinkItems[2].icon} name={LinkItems[2].name} path={LinkItems[2].path} />
+      <Flex alignItems={'center'} justifyContent={'flex-end'}>
+        <Heading textAlign={'center'} fontSize={24}>COLLAB-MESSENGER</Heading>
+      </Flex>
+      <Flex mt={{ base: 5, md: 5 }} alignItems='center' justifyContent={'space-around'}>
+        <Button ><BsChatTextFill size={30} /></Button>
+        <Button ><RiTeamFill size={30} /></Button>
+      </Flex>
+      <Box minW={'210px'}>
+        <NavItem key={LinkItems[0].name} icon={LinkItems[0].icon} name={LinkItems[0].name} path={LinkItems[0].path} />
+        <UserTeams />
+        <NavItem key={LinkItems[2].name} icon={LinkItems[2].icon} name={LinkItems[2].name} path={LinkItems[2].path} />
+      </Box>
     </Box >
   );
 }
