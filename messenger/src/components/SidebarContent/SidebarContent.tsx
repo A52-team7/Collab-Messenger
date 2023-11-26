d80a02c1f619661742cf5e3c6667af8b2f9a176e
Mihail Uymaz <michael.m.uymaz@gmail.com>
2023-11-26 03:26:11 +0200

    feat: SidebarContent
    Hover and active colors added. When route is not on home, home btn is not colored as clicked.

--- messenger/src/components/SidebarContent/SidebarContent.tsx
+++ messenger/src/components/SidebarContent/SidebarContent.tsx
@@ -1,6 +1,7 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import AppContext from '../../context/AppContext';
-import NavItem from '../NavItem/NavItem';
+// import NavItem from '../NavItem/NavItem';
 import {
   Box,
   Flex,
@@ -9,7 +10,6 @@ import {
   useColorModeValue,
   Heading,
   FormLabel,
-  Center
 } from '@chakra-ui/react';
 import {
   FiHome,
@@ -21,13 +21,12 @@ import { SIDEBAR_SHOW_MESSAGES, SIDEBAR_SHOW_TEAMS } from '../../common/constant
 
 import UserTeams from '../UserTeams/UserTeams'
 import MyChatsSideNavBar from '../MyChatsSideNavBar/MyChatsSideNavBar';
-import { useNavigate } from 'react-router-dom';
 
-const LinkItems = [
-  { name: 'Home', icon: FiHome, path: '/' },
-  { name: 'Team', icon: FiHome, path: '/new-team' },
-  { name: 'Create new chat', icon: FiHome, path: '/create-new-chat' },
-];
+// const LinkItems = [
+//   { name: 'Home', icon: FiHome, path: '/' },
+//   { name: 'Team', icon: FiHome, path: '/new-team' },
+//   { name: 'Create new chat', icon: FiHome, path: '/create-new-chat' },
+// ];
 
 interface SidebarContentProps {
   onClose: () => void
@@ -36,13 +35,29 @@ interface SidebarContentProps {
 
 const SidebarContent = ({ onClose, ...rest }: SidebarContentProps) => {
   const { user } = useContext(AppContext);
-  const [show, setShow] = useState(SIDEBAR_SHOW_TEAMS);
+  const [show, setShow] = useState(SIDEBAR_SHOW_MESSAGES);
+  const [activeBtn, setActiveBtn] = useState('');
+  const [homeActiveBtn, setHomeActiveBtn] = useState(false);
   const navigate = useNavigate();
+  const location = useLocation();
 
   const showContentHandle = (e: React.MouseEvent<HTMLElement>) => {
     const targetId = e.currentTarget.id;
-    setShow(targetId === 'messages-btn' ? SIDEBAR_SHOW_MESSAGES : SIDEBAR_SHOW_TEAMS);
+    if (targetId === 'home-btn') {
+      navigate('/');
+    } else {
+      setShow(targetId === 'messages-btn' ? SIDEBAR_SHOW_MESSAGES : SIDEBAR_SHOW_TEAMS);
+    }
+    setActiveBtn(targetId);
   }
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      setHomeActiveBtn(true);
+    } else {
+      setHomeActiveBtn(false);
+    }
+  }, [location]);
 
   return (
     <Box
@@ -63,31 +78,37 @@ const SidebarContent = ({ onClose, ...rest }: SidebarContentProps) => {
       {user &&
         <>
           <Flex mt={{ base: 5, md: 5 }} alignItems='center' justifyContent={'space-around'}>
-            <Box
-              cursor={'pointer'}
-              onClick={() => navigate('/')}>
-              <Button px={3} py={6}>
+            <Box>
+              <Button px={3} py={6}
+                id={'home-btn'}
+                bg={homeActiveBtn ? 'cyan' : 'gray.100'}
+                _hover={{ opacity: 0.5 }}
+                onClick={(e) => showContentHandle(e)}>
                 <FiHome size={30} />
               </Button>
-              <FormLabel textAlign={'center'}>HOME</FormLabel>
+              <FormLabel htmlFor={'home-btn'}>HOME</FormLabel>
             </Box>
           </Flex>
           <Flex alignItems='center' justifyContent={'space-around'}>
             <Box>
               <Button borderRadius={'50%'} px={3} py={6}
                 id={'messages-btn'}
+                bg={activeBtn === 'messages-btn' ? 'green.300' : 'gray.100'}
+                _hover={{ opacity: 0.5 }}
                 onClick={(e) => showContentHandle(e)}>
                 <BsChatTextFill size={30} />
               </Button>
-              <FormLabel>CHATS</FormLabel>
+              <FormLabel htmlFor={'messages-btn'}>CHATS</FormLabel>
             </Box>
             <Box>
               <Button rounded={'xl'} px={3} py={6}
                 id={'teams-btn'}
+                bg={activeBtn === 'teams-btn' ? 'green.300' : 'gray.100'}
+                _hover={{ opacity: 0.5 }}
                 onClick={(e) => showContentHandle(e)}>
                 <RiTeamFill size={30} />
               </Button>
-              <FormLabel>TEAMS</FormLabel>
+              <FormLabel htmlFor={'teams-btn'}>TEAMS</FormLabel>
             </Box>
           </Flex>
           <Box minW={'210px'}>
