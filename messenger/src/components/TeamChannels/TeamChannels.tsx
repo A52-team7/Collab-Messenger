8b7df3872cf82ef23254279ed72b18d7e83ea5b0
Mihail Uymaz <michael.m.uymaz@gmail.com>
2023-12-02 22:04:45 +0200

    reverse: reversing loading state; feat: seen on team channel - partial

--- messenger/src/components/TeamChannels/TeamChannels.tsx
+++ messenger/src/components/TeamChannels/TeamChannels.tsx
@@ -1,60 +1,73 @@
-import { useContext, useState,useEffect } from 'react';
-import {getChannelById} from '../../services/channels.service';
-import {Channel} from '../MyChatsSideNavBar/MyChatsSideNavBar';
-import {getTeamChannelsLive, getTeamById} from '../../services/teams.service'
-import {Button, Box} from '@chakra-ui/react';
-import AppContext, {UserState} from '../../context/AppContext'
+import { useContext, useState, useEffect } from 'react';
+import { getChannelById, setChannelToSeen } from '../../services/channels.service';
+import { Channel } from '../MyChatsSideNavBar/MyChatsSideNavBar';
+import { getTeamChannelsLive, getTeamById } from '../../services/teams.service'
+import { Button, Box } from '@chakra-ui/react';
+import AppContext, { UserState } from '../../context/AppContext'
 import { useNavigate } from "react-router-dom"
-import {Team} from '../CreateTeam/CreateTeam'
+import { Team } from '../CreateTeam/CreateTeam'
 
 
-export interface Id{
-    id:string
-  }
+export interface Id {
+  id: string
+}
 
-const TeamChannels = ({id}: Id) =>{
-    const [channels, setChannels] = useState<Channel[]>([])
-    const { userData } = useContext<UserState>(AppContext);
-    const navigate = useNavigate();
-    const [team,setTeam] = useState<Team>({  
+const TeamChannels = ({ id }: Id) => {
+  const [channels, setChannels] = useState<Channel[]>([])
+  const { userData } = useContext<UserState>(AppContext);
+  const navigate = useNavigate();
+  const [team, setTeam] = useState<Team>({
     id: '',
     name: '',
     owner: '',
     members: {},
     description: '',
-    generalChannel:'',})
+    generalChannel: '',
+  })
 
-    useEffect(() => {
+  useEffect(() => {
 
-        getTeamChannelsLive(id, (data: string[]) => {
-            Promise.all(
-            data.map((channelId: string) =>{
-            return getChannelById(channelId)      
-             } ))
-            .then(elChannel => {
-               setChannels([...elChannel])
-            })
-            .catch(e => console.log(e))
+    getTeamChannelsLive(id, (data: string[]) => {
+      Promise.all(
+        data.map((channelId: string) => {
+          return getChannelById(channelId)
+        }))
+        .then(elChannel => {
+          setChannels([...elChannel])
         })
-        },[])
+        .catch(e => console.log(e))
+    })
+  }, [])
 
-    useEffect(() => {
-        getTeamById(id)
-        .then(res => setTeam(res))
-    },[])
-        
-       
-    return (
-        <>
-        {channels.map((channel: Channel) => {
+  useEffect(() => {
+    getTeamById(id)
+      .then(res => setTeam(res))
+  }, [])
+
+  const handleOpenChannel = (id: string) => {
+    if (userData) {
+      navigate('/chat', { state: { channelId: id, team: team } })
+      setChannelToSeen(id, userData?.handle);
+    }
+  };
+
+  return (
+    <>
+      {channels.map((channel: Channel) => {
+        let userHasSeen: boolean = false;
+        Object.entries(channel.seenBy).map(([key, value]) => {
+          if (key === userData?.handle && value === true) userHasSeen = true;
+        });
+
         return (<Box key={channel.id}>
-        <Button variant='ghost' onClick={() => {navigate('/chat', { state: { channelId: channel.id, team: team } })}}>
-        {channel.title} 
-        </Button>
+          <Button bg={userHasSeen ? 'green' : 'red'} variant='ghost' onClick={() => handleOpenChannel(channel.id)}>
+            {channel.title} ({userHasSeen ? 'seen' : 'not seen'})
+          </Button>
         </Box>
-        )})}
-        </>
-    )
+        )
+      })}
+    </>
+  )
 }
 
 export default TeamChannels;
