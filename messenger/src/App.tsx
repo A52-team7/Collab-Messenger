46fc8815df0d78061ea910f4195210d8373f66b9
Mihail Uymaz <michael.m.uymaz@gmail.com>
2023-12-02 22:05:50 +0200

    reverse: reversing loading state; feat: seen on team channel - partial

--- messenger/src/App.tsx
+++ messenger/src/App.tsx
@@ -18,19 +18,15 @@ import EditTeamInfo from './components/EditTeamInfo/EditTeamInfo';
 
 function App(): JSX.Element {
   // loading, error
-  const [userAuth, loading] = useAuthState(auth);
+  const [userAuth] = useAuthState(auth);
   // this hook will check storage for user credentials (eventually stored during user login with firebase functions) and retrieve the actual authentication state (user will either be null or the user object from the last persisted login)
   // the loading status will be true while the hooks retrieves the status of the user and will be set to false when the user has been retrieved (object or null)
   // the error will be set only when specific problem with the auth state is detected
   const [appState, setAppState] = useState<UserState>({
     user: null,
     userData: null,
-    loading: loading,
     setContext: () => { },
   });
-
-  console.log('I am reloading');
-
 
   // update the user in the app state to match the one retrieved from the hook above
   if (appState.user !== userAuth) {
@@ -53,7 +49,6 @@ function App(): JSX.Element {
         setAppState({
           ...appState,
           userData: snapshot.val()[Object.keys(snapshot.val())[0]],
-          loading: false
         });
       })
       .catch(e => alert(e.message));
@@ -61,23 +56,20 @@ function App(): JSX.Element {
 
   return (
     <AppContext.Provider value={{ ...appState, setContext: setAppState }}>
-      {
-        !appState.loading &&
-        <Body>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/register' element={!appState.user && <Register />} />
-            <Route path='/login' element={!appState.user && <Login />} />
-            <Route path='/new-chat' element={<AuthenticatedRoute><NewChat /></AuthenticatedRoute>} />
-            <Route path='/chat' element={<AuthenticatedRoute><Chat /></AuthenticatedRoute>} />
-            <Route path='/user-details' element={<AuthenticatedRoute><UserDetails /></AuthenticatedRoute>} />
-            {/* <Route path='/search' element={<AuthenticatedRoute><SearchPage /></AuthenticatedRoute>} /> */}
-            <Route path='/new-team' element={<AuthenticatedRoute><CreateTeam /></AuthenticatedRoute>} />
-            <Route path='/edit-team-information' element={<AuthenticatedRoute><EditTeamInfo /></AuthenticatedRoute>} />
-            <Route path='*' element={<NoPageFound />} />
-          </Routes>
-        </Body>
-      }
+      <Body>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={!appState.user && <Register />} />
+          <Route path='/login' element={!appState.user && <Login />} />
+          <Route path='/new-chat' element={<AuthenticatedRoute><NewChat /></AuthenticatedRoute>} />
+          <Route path='/chat' element={<AuthenticatedRoute><Chat /></AuthenticatedRoute>} />
+          <Route path='/user-details' element={<AuthenticatedRoute><UserDetails /></AuthenticatedRoute>} />
+          {/* <Route path='/search' element={<AuthenticatedRoute><SearchPage /></AuthenticatedRoute>} /> */}
+          <Route path='/new-team' element={<AuthenticatedRoute><CreateTeam /></AuthenticatedRoute>} />
+          <Route path='/edit-team-information' element={<AuthenticatedRoute><EditTeamInfo /></AuthenticatedRoute>} />
+          <Route path='*' element={<NoPageFound />} />
+        </Routes>
+      </Body>
     </AppContext.Provider>
   );
 }
