899d1576b5fa1147046571ac87201843a2db49fb
Mihail Uymaz <michael.m.uymaz@gmail.com>
2023-12-13 03:13:27 +0200

    css: send button tooltip removed

--- messenger/src/components/SendButton/SendButton.tsx
+++ messenger/src/components/SendButton/SendButton.tsx
@@ -1,4 +1,4 @@
-import { Button, Tooltip } from '@chakra-ui/react';
+import { Button } from '@chakra-ui/react';
 import { useState } from 'react';
 import { BsSend } from "react-icons/bs";
 import { BsFillSendFill } from "react-icons/bs";
@@ -20,20 +20,18 @@ const SendButton = ({ onSendMessage }: SendButtonProps) => {
   }
 
   return (
-    <Tooltip hasArrow border={'1px solid black'} label={'Send message'} bg={'rgb(237,254,253)'} color='black'>
-      <Button
-        ml={-5}
-        bg={'none'}
-        color={'white'}
-        flex={'1 0 auto'}
-        onMouseEnter={onSeeColor}
-        onMouseLeave={onHideColor}
-        _hover={{ bg: 'none' }}
-        _focus={{ bg: 'none' }}
-        onClick={onSendMessage}>
-        {!visibleColor ? (<BsSend size={35} />) : (<BsFillSendFill size={35} />)}
-      </Button>
-    </Tooltip>
+    <Button
+      ml={-5}
+      bg={'none'}
+      color={'white'}
+      flex={'1 0 auto'}
+      onMouseEnter={onSeeColor}
+      onMouseLeave={onHideColor}
+      _hover={{ bg: 'none' }}
+      _focus={{ bg: 'none' }}
+      onClick={onSendMessage}>
+      {!visibleColor ? (<BsSend size={35} />) : (<BsFillSendFill size={35} />)}
+    </Button>
   )
 }
 
