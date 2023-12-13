f3bda1738ec12629745a6a5c069e0faab746ac26
hristina-georgieva <hristinag28@gmail.com>
2023-12-13 13:12:20 +0200

    css: home page

--- messenger/src/views/Home/Home.tsx
+++ messenger/src/views/Home/Home.tsx
@@ -1,17 +1,23 @@
-import { Box,  } from '@chakra-ui/react';
-import MyCalendar from '../../components/MyCalendar/MyCalendar';
-// import JitsiMeetingComponent from '../../components/JitsiMeetingComponent/JitsiMeetingComponent';
+import { Box, Center, Image, Text } from '@chakra-ui/react';
 
 
 const Home = (): JSX.Element  =>{
 
     return (
-        <Box backgroundImage="url('/OIG.png')"
-        backgroundPosition="center"
-        backgroundRepeat="no-repeat"
-        backgroundSize='cover' >
-            {/* <JitsiMeetingComponent/> */}
-        <MyCalendar/>
+        // <Box backgroundImage="url('/home_background.jpg')"
+        // backgroundPosition="center"
+        // backgroundRepeat="no-repeat"
+        // backgroundSize='cover'
+        // position="fixed"
+        // h={'95%'}
+        // w={'85%'}
+        // // zIndex="1"
+        // >
+        // </Box>
+        <Box h={'100%'} mt={'12%'}>
+            <Center>
+                <Image src={'/home.png'} h={'400px'} box-shadow={ '5px 4px 8px rgba(0, 0, 0, 0.1)'}/>
+            </Center>
         </Box>
     )
 }
