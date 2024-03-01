2020f2b8893f548f18887f6c587b284f4262ca5f
Mihail Uymaz <michael.m.uymaz@gmail.com>
2024-03-01 12:43:26 +0200

    format

--- messenger/src/components/ImageModal/ImageModal.tsx
+++ messenger/src/components/ImageModal/ImageModal.tsx
@@ -2,33 +2,32 @@ import { Button, Center, Image, Modal, ModalBody, ModalCloseButton, ModalContent
 
 
 interface ImageModalProps {
-    imageSrc: string;
-  }
+  imageSrc: string;
+}
 
 const ImageModal = ({imageSrc}: ImageModalProps) => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
-    const { isOpen, onOpen, onClose } = useDisclosure()
-
-    return (
-              <>
-                <Button onClick={onOpen} h={'350px'} bg={'none'} _hover={{ bg: 'none' }}>
-                    <Image src={imageSrc} h={'350px'}/>
-                </Button>
+  return (
+    <>
+      <Button onClick={onOpen} h={'350px'} bg={'none'} _hover={{ bg: 'none' }}>
+        <Image src={imageSrc} h={'350px'} />
+      </Button>
           
-                <Modal isOpen={isOpen} onClose={onClose}>
-                  <ModalOverlay />
-                  <ModalContent maxW="1000px" maxH="600px" bg={'rgb(237,254,253)'}>
-                    <ModalHeader />
-                    <ModalCloseButton />
-                    <ModalBody>
-                        <Center>
-                            <Image src={imageSrc} maxH="500px"/>
-                        </Center>
-                    </ModalBody>
-                  </ModalContent>
-                </Modal>
-              </>          
-    )
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent maxW="1000px" maxH="600px" bg={'rgb(237,254,253)'}>
+          <ModalHeader />
+          <ModalCloseButton />
+          <ModalBody>
+            <Center>
+              <Image src={imageSrc} maxH="500px" />
+            </Center>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 }
 
 export default ImageModal;
